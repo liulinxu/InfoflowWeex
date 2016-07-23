88a5d7c66a0b69ea0814eafdb650f9b20b7492ec
liulinxu <775873824@qq.com>
2016-07-23 17:40:20 +0800

    test5

--- app/src/main/java/com/example/liulinxu/infoflowweex/MainActivity.java
+++ app/src/main/java/com/example/liulinxu/infoflowweex/MainActivity.java
@@ -8,9 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.taobao.weex.IWXRenderListener;
+import com.taobao.weex.WXSDKInstance;
+import com.taobao.weex.common.WXRenderStrategy;
 
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements IWXRenderListener{
+    private final String TAG = "MainActivity";
+    private RelativeLayout mContentPanel;
+    private WXSDKInstance mInstance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ///
+        mInstance = new WXSDKInstance(this);
+        mContentPanel = (RelativeLayout) findViewById(R.id.contentPanel);
+
+        loadFromService();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +40,63 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        mInstance.registerRenderListener(this);
+    }
+
+    private void loadFromLocal(){
+
+    }
+
+    private void loadFromService(){
+        /**
+         * 在父容器不为window时，不知道会不会有问题，因为oncreate方法中时不知道view的宽高的。
+         * 如果放到后面会不会影响页面的加载速度
+         */
+        mInstance.renderByUrl(TAG,"http://100.84.253.100:8081/weex_tmp/h5_render/tech_list.js",new HashMap<String, Object>(),
+                null,
+                getWindow().getDecorView().getWidth(),
+                getWindow().getDecorView().getHeight(),
+                WXRenderStrategy.APPEND_ASYNC);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(mInstance!=null){
+            mInstance.onActivityStart();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(mInstance!=null){
+            mInstance.onActivityResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mInstance!=null){
+            mInstance.onActivityPause();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mInstance!=null){
+            mInstance.onActivityStop();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mInstance!=null){
+            mInstance.onActivityDestroy();
+        }
     }
 
     @Override
@@ -48,5 +119,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onViewCreated(WXSDKInstance instance, View view) {
+        mContentPanel.addView(view);
+    }
+
+    @Override
+    public void onRenderSuccess(WXSDKInstance instance, int width, int height) {
+
+    }
+
+    @Override
+    public void onRefreshSuccess(WXSDKInstance instance, int width, int height) {
+
+    }
+
+    @Override
+    public void onException(WXSDKInstance instance, String errCode, String msg) {
+
     }
 }
